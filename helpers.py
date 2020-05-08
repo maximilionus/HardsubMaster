d3ac55844b0c25c78fafab160332126cb0a0e199
maximilionus <xxxarakxxx@gmail.com>
2020-05-08 03:08:42 +0300

    Overhaul upgrade 080520200308 - Added paths handling for dragging files bullsh... - Config .json added with path to ffmpeg executable - Renamed some variables to reflect their content

--- helpers.py
+++ helpers.py
@@ -1,9 +1,48 @@
-from os import path, listdir, system
-from sys import platform
+from os import path, listdir, system, getcwd, chdir
+from sys import platform, argv
+import json
 
 NAME_STR = "Hardsub Master"
 BUILD_STR = "07052020-2249"
 TITLE_STR = f"title {NAME_STR} by @maximilionus (build {BUILD_STR})"
+CONFIG_DEFAULT = {
+	"ffmpeg_path": path.abspath("./ffmpeg/ffmpeg.exe") if (path.exists('./ffmpeg/ffmpeg.exe') and platform == 'win32') else "ffmpeg"
+}
+EXECUTABLE_LOCATION = path.dirname(argv[0])
 
 def returnMKVs(files: list):
-	return [path.basename(file) for file in files if path.basename(file).endswith('.mkv')]
+	return [f for f in files if path.basename(f).endswith('.mkv')]
+
+class HMDirHandler:
+	def __init__(self):
+		if not getcwd() == EXECUTABLE_LOCATION: chdir(EXECUTABLE_LOCATION)
+		self._executable_path = EXECUTABLE_LOCATION
+		self._filedir_path = ''
+
+	def switchDir(self, value: int):
+		"""
+		Switch work directory.
+
+		Arguments:
+			value {int} -- 0 - switch to executable path, 1 - switch to file path.
+		"""
+		if value == 0: chdir(self._executable_path)
+		elif value == 1 : chdir(self._filedir_path)
+
+	def updateFileDir(self, path: str):
+		self._filedir_path = path
+
+class HMConfig:
+	def __init__(self):
+		if not self.exists():
+			with open('./config.json', 'wt') as f:
+				json.dump(CONFIG_DEFAULT, f, indent=4)
+		with open('./config.json', 'rt') as f:
+			self._config = json.load(f)
+
+	def getFFmpegPath(self) -> str:
+		return self._config["ffmpeg_path"]
+
+	@staticmethod
+	def exists():
+		return path.exists('./config.json')
--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-import sys
-from os import path, mkdir, system
+import sys, os
 import subprocess
-from termcolor import cprint
+from termcolor import cprint, colored
 import helpers as h
 
-def burn_subtitles(mkv_file: str, output_dir: str, subtitlesFile: str):
-	subprocess.call(f'ffmpeg -i "{mkv_file}" -c:v libx264 -crf 18 -c:a copy -b:a 128k -vf subtitles={subtitlesFile} {path.join(output_dir, mkv_file)}')
+def burn_subtitles(mkv_file_path: str, output_dir: str, ffmpeg: str):
+	mkv_file = os.path.basename(mkv_file_path)
+	subprocess.call(f'ffmpeg -i "{mkv_file}" -c:v libx264 -crf 18 -c:a copy -b:a 128k -vf subtitles={mkv_file} {os.path.join(output_dir, mkv_file)}')
 	cprint(f'>> {mkv_file} << DONE', color='green', attrs=['reverse'])
 
-if sys.platform == 'win32': system("color"); system(h.TITLE_STR)
+# Init
+hmdir = h.HMDirHandler()
+hmconfig = h.HMConfig()
+
+if sys.platform == 'win32': os.system("color"); os.system(h.TITLE_STR)
 if len(sys.argv) == 1: cprint('Not enough files', color='red', attrs=['reverse']); input('PRESS ENTER TO EXIT'); sys.exit()
 
-mkv_files = h.returnMKVs(sys.argv[1:])
-for mkv_file in mkv_files:
+# Begin merging
+mkv_files_paths = h.returnMKVs(sys.argv[1:])
+for mkv_file_path in mkv_files_paths:
+	hmdir.updateFileDir(os.path.dirname(mkv_file_path))
+	hmdir.switchDir(1)
 	output_dir = './output'
-	if not path.exists(output_dir): mkdir(output_dir)
-	burn_subtitles(mkv_file=mkv_file, output_dir=output_dir, subtitlesFile=sys.argv[sys.argv.index('--subtitles') + 1] if '--subtitles' in sys.argv else mkv_file)
+	if not os.path.exists(output_dir): os.mkdir(output_dir)
+	burn_subtitles(mkv_file_path=mkv_file_path, output_dir=output_dir, ffmpeg=hmconfig.getFFmpegPath())
 
-cprint(f'{len(mkv_files)} files processed', color='yellow', attrs=['reverse'])
-input('\aPRESS ENTER TO EXIT')
+cprint(f'{len(mkv_files_paths)} files processed', color='green', attrs=['reverse'])
+input('\a' + colored('PRESS ENTER TO EXIT', color='red'))
